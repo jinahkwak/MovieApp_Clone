1af86adc881d4d1dfceab90e99dddd427454cae2
jinahkwak <wlsdk8949@naver.com>
2020-09-05 03:40:24 +0900

    dynamic data allocation ex

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
-function Fruit({ favourite }) {
-  return <h3>I like { favourite }</h3>;
+function Fruit({ name, picture}) {
+  return <div>
+      <h2>I like {name}</h2>
+      <img src = {picture}></img>
+  </div>
 }
 
+const fruitILike = [{
+  name: "apple",
+  image: "https://i5.walmartimages.ca/images/Large/094/514/6000200094514.jpg"
+},
+ 
+  {
+  name: "melon",
+  image: "https://sc02.alicdn.com/kf/UTB83so5mnzIXKJkSafVq6yWgXXaf.jpg"
+}];
+
 function App() {
-  return <div>Hello!!
-      <Fruit favourite="apple" />
-      <Fruit favourite="melon" />
-      <Fruit favourite="grape" />
-      <Fruit favourite="strawberry" />
+  return <div>
+      {fruitILike.map(dish => <Fruit name={dish.name} picture={dish.image}/>
+      )}
   </div>;
 
 }
