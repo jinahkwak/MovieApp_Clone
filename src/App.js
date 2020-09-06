e3aa90cd3c6252056884feb96fa7b2e759b05f01
jinahkwak <wlsdk8949@naver.com>
2020-09-06 16:30:22 +0900

    react state render loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
 
-function Fruit({ name, picture}) {
-  return <div>
-      <h2>I like {name}</h2>
-      <img src = {picture}></img>
-  </div>
-}
-
-const fruitILike = [{
-  name: "apple",
-  image: "https://i5.walmartimages.ca/images/Large/094/514/6000200094514.jpg"
-},
- 
-  {
-  name: "melon",
-  image: "https://sc02.alicdn.com/kf/UTB83so5mnzIXKJkSafVq6yWgXXaf.jpg"
-}];
-
-function App() {
-  return <div>
-      {fruitILike.map(dish => <Fruit name={dish.name} picture={dish.image}/>
-      )}
-  </div>;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount() {
+    setTimeout(() =>{
+      this.setState({ isLoading: false, book: true});
+    }, 6000);
+  }
+  render() {
+    const { isLoading } = this.state;
+  return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+  }
 
 }
 
